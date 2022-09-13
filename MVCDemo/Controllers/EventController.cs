ed23726695d5a3ab1b92538931145b79576a42fb
Shishir <shishiralva20s@gmail.com>
2022-09-13 09:27:03 +0530

    Add project files.

---
+++ MVCDemo/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EventController : Controller
+    {
+
+        public List<Booking> events { get; set; }
+        
+        public IActionResult Index()
+        {
+            events = new List<Booking>();
+            events.Add (new Booking { Title = "ask Ceo", Date = DateTime.Today, Quantity = 20 });
+            events.Add  (new Booking { Title = "Alan walker", Date = DateTime.Today, Quantity = 30 });
+            
+            return View(events);
+        }
+    }
+}
---
+++ WebAppDETAug2022/Servics/IHelo.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAppDETAug2022.Servics
+{
+    public interface IHelo
+    {
+        string SayHello(string name);
+
+    }
+
+    public class Helo1 : IHelo
+    {
+        public string SayHello( string name)
+        {
+            return $"Hello {name}, Welcome to ASP.net Core";
+        }
+    }
+
+    public class Helo2 : IHelo
+    {
+        public string SayHello(string name)
+        {
+            return $"Hello,Hai {name}, Howz the day!!";
+
+        }
+    }
+}
